d3720f89061f8b63d2951ecfa532a5d3ee06a586
devilb2015 <devilb2015@gmail.com>
2021-10-13 23:51:00 +0900

    elasticsearch 추가

--- ssac_backend/routes/lov/index.js
+++ ssac_backend/routes/lov/index.js
@@ -4,9 +4,11 @@ var router = express.Router();
 const authRouter = require("./auth/index");
 const postRouter = require("./post/index");
 const commentRouter = require("./comment/index");
+const searchRouter = require("./search/index");
 
 router.use("/auth", authRouter);
 router.use("/post", postRouter);
 router.use("/comment", commentRouter);
+router.use("/search", searchRouter);
 
 module.exports = router;
